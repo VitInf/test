d5199bb2ec1ed195dab44e437b4cee68837414b2
VitInf <95320624+VitInf@users.noreply.github.com>
2021-12-07 22:03:35 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print('Hello!')
+class Flight():
+    def __init__(self, capasity):
+        self.capasity = capasity
+        self.passengers = []
+    
+    def add_passengers(self, name):
+        if len(self.passengers)> self.capasity:
+            print(f"Вільних місць для {name} немає")
+        else:
+            self.passengers.append(name)
+
+f = Flight(3)
+people = ["Andriy","Roma","Lioha","Max"]
+
+for person in people:
+    f.add_passengers(person)
+    
